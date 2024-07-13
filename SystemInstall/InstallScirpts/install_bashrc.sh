e632e19418cd756343c84423189b8ca3292162fa
ysyx_22050928-zheng zhong qiang <3486829357@qq.com>
2024-07-13 22:34:19 +0800

    添加了针对不同系统的配置

--- SystemInstall/InstallScirpts/install_bashrc.sh
+++ SystemInstall/InstallScirpts/install_bashrc.sh
@@ -15,8 +15,8 @@ eg：                                                                    *
     2.                                                                  *\n
 其他信息：                                                              *
 *************************************************************************\n"
-parameter_names=(1 2 3 4 5 6) #输入参数的名称列表
-parameter_num=0  # 脚本一定需要传入的参数个数
+parameter_names=(ubuntu/wsl 2 3 4 5 6) #输入参数的名称列表
+parameter_num=1  # 脚本一定需要传入的参数个数
 shell_script_path=/home/zzq/Code/Sheel/   # 脚本待调用的脚本的父目录
 son_script_path=makefiles  # 待调用的脚本的子目录
 call_script_path="$shell_script_path""$son_script_path"
@@ -35,12 +35,16 @@ done
 
 #***************配置bushrc***************************
 zzq_config_name=.zzq_config
-zzq_config_path="~/.zzq_config"
-zzq_ubuntu_config_path="$zzq_config_path""/bashrc/special_bashrc/ubuntu"
-zzq_wsl_config_path="$zzq_config_path""/bashrc/special_bashrc/wsl"
-zzq_config_repo_path=git@github.com:yunzhong8/ZzqLinuxConfig.git
-sudo rm -rf $zzq_config_path
-sudo mkdir -p  $zzq_config_path
+zzq_config_path="\${HOME}/.zzq_config"
+zzq_basic_bashrc_config_path="$zzq_config_path""/bashrc"
+zzq_ubuntu_bashrc_config_path="$zzq_config_path""/bashrc/special_bashrc/ubuntu"
+zzq_wsl_bashrc_config_path="$zzq_config_path""/bashrc/special_bashrc/wsl"
+zzq_config_repo_path="git@github.com:yunzhong8/ZzqLinuxConfig.git"
+
+chmod -R 755 "$zzq_config_path"  # 设置文件夹及其内容为普通用户可读写执行
+
+rm -rf $zzq_config_path
+mkdir -p  $zzq_config_path
 
 pushd $HOME #进入用户目录
 
@@ -73,9 +77,11 @@ WSL_SPECIAL_CONFIG=\"true\"
 "
 fi
 
+echo "$bashrc_config_code" >> ~/.bashrc
+
 # 设置bashrc中引用我的代码
 bashrc_add_code="# 配置文件目录
-CONFIG_DIR=\"$zzq_config_path\"
+CONFIG_DIR=\"$zzq_basic_bashrc_config_path\"
 
 # 导入基本bashrc配置（所有平台通用）
 if [ -d \"\$CONFIG_DIR\" ]; then
@@ -90,7 +96,7 @@ fi
 
 #导入Ubuntu平台的bashrc配置
 if [ \"\$UBUNTU_SPECIAL_CONFIG\" = \"true\" ]; then
-	UBUNTU_CONFIG_DIR=\"$zzq_ubuntu_config_path\"
+	UBUNTU_CONFIG_DIR=\"$zzq_ubuntu_bashrc_config_path\"
 	# 导入基本bashrc配置（所有平台通用）
 	if [ -d \"\$UBUNTU_CONFIG_DIR\" ]; then
 	    for file in "\$UBUNTU_CONFIG_DIR"/*.sh; do
@@ -105,7 +111,7 @@ fi
 
 #导入WSL平台的bashrc配置
 if [ \"\$WSL_SPECIAL_CONFIG\" = \"true\" ]; then
-	WSL_CONFIG_DIR=\"$zzq_wsl_config_path\"
+	WSL_CONFIG_DIR=\"$zzq_wsl_bashrc_config_path\"
 	# 导入基本bashrc配置（所有平台通用）
 	if [ -d \"\$WSL_CONFIG_DIR\" ]; then
 	    for file in "\$UBUNTU_CONFIG_DIR"/*.sh; do
@@ -127,4 +133,4 @@ fi
 #   echo "Content already exists in ~/.bashrc"
 # fi
 echo "$bashrc_add_code" >> ~/.bashrc
-source ~/.bashrc
+source ${HOME}/.bashrc
--- SystemInstall/InstallScirpts/install_zzq_scirpt.sh
+++ SystemInstall/InstallScirpts/install_zzq_scirpt.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
+###
+ # @Author: ysyx_22050928-zheng zhong qiang 3486829357@qq.com
+ # @Date: 2024-07-13 13:21:19
+ # @LastEditors: ysyx_22050928-zheng zhong qiang 3486829357@qq.com
+ # @LastEditTime: 2024-07-13 20:09:52
+ # @FilePath: /LinuxConfigInstall/SystemInstall/InstallScirpts/install_zzq_scirpt.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+###
 #
 # install_c_lib.sh
 # Copyright (C) 2024 zzq <zzq@zzq-HP-Pavilion-Gaming-Laptop-15-cx0xxx>
@@ -15,8 +23,9 @@ eg：                                                                    *
     2.                                                                  *\n
 其他信息：                                                              *
 *************************************************************************\n"
-parameter_names=(1 2 3 4 5 6)
-parameter_num=0
+set -x
+parameter_names=(ubuntu/wsl 2 3 4 5 6)
+parameter_num=1
  #输入参数的名称列表
 
 #检测参数是否正确传入
@@ -30,8 +39,23 @@ do
     fi
 done
 
-# 遍历脚本目录下的全部文件，设置文件的权限是可执行
+# 加载用户的环境变量配置文件
+source ~/.bashrc  # 或者 source ~/.profile
+
 
 #  EXE_STARDICT_PATH 这个环境变量设置在配置文件中的.mybashrc中
 # 将脚本目录下的全部脚本赋予执行全
-chmod -R +x $EXE_STARDICT_PATH
+
+chmod -R +x $OVERALL_EXE_BASIC_SCRIPT_PATH
+
+platform=$1
+
+
+if [ "$platform" = "ubuntu" ]; then
+    echo "目前平台是："$platform
+    echo $OVERALL_EXE_UBUNTU_SCRIPT_PATH
+#    chmod -R +x $OVERALL_EXE_UBUNTU_SCRIPT_PATH
+else
+    echo "目前平台是："$platform
+#    chmod -R +x $OVERALL_EXE_WSL_SCRIPT_PATH
+fi
